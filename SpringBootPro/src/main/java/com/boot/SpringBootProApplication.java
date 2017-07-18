ebdcae9dc77f19de6cc57912b0a430a9626dc7d5
zhenyu7.16 <260787342@qq.com>
2017-07-18 17:34:08 +0800

    bootsrtap

--- SpringBootPro/src/main/java/com/boot/SpringBootProApplication.java
+++ SpringBootPro/src/main/java/com/boot/SpringBootProApplication.java
@@ -3,12 +3,23 @@ package com.boot;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 @MapperScan("com.boot.dao.mapper")
+@Controller
 public class SpringBootProApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootProApplication.class, args);
 	}
+	
+	
+	 @RequestMapping("/")
+	    public String index(Model model){
+	        
+	        return "index";
+	    }
 }
