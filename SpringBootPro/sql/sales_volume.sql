faf9bde00e2281c988d8ada2d7b6980a941c7da2
zhenyu7.16 <260787342@qq.com>
2017-07-20 17:47:50 +0800

    修改建表语句

--- SpringBootPro/sql/sales_volume.sql
+++ SpringBootPro/sql/sales_volume.sql
@@ -20,19 +20,26 @@ USE `dbgirl` ;
 -- -----------------------------------------------------
 -- Table `mydb`.`sales_volume`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `dbgirl`.`sales_volume` (
-  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `classcation` VARCHAR(45) NOT NULL COMMENT '类别：【总】、【vivo】、【oppo】',
-  `date` VARCHAR(10) NOT NULL COMMENT 'yyyy-MM保存，展示需要显示成yyyy年MM月',
-  `datatime` VARCHAR(45) NOT NULL COMMENT '生成该记录的日期 yyyy-MM-dd HH:mm:ss',
-  `amount` INT NOT NULL DEFAULT 0 COMMENT '数量',
-  `regioname` VARCHAR(55) NOT NULL COMMENT '地区name',
-  `modifydate` DATETIME NULL COMMENT '记录修改的日期',
+
+
+
+CREATE TABLE  IF NOT EXISTS   `dbgirl`.`sales_volume` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `classcation` varchar(45) NOT NULL COMMENT '类别：【总】、【vivo】、【oppo】',
+  `date` varchar(10) NOT NULL COMMENT 'yyyy-MM保存，展示需要显示成yyyy年MM月',
+  `datatime` varchar(45) DEFAULT NULL COMMENT '生成该记录的日期 yyyy-MM-dd HH:mm:ss',
+  `amount` int(11) NOT NULL DEFAULT '0' COMMENT '数量',
+  `regioname` varchar(55) NOT NULL COMMENT '地区name',
+  `modifydate` datetime DEFAULT NULL COMMENT '记录修改的日期',
   PRIMARY KEY (`id`),
-  INDEX `classcation` (`classcation` ASC),
-  INDEX `date` (`date` ASC),
-  INDEX `regioname` (`regioname` ASC))
-ENGINE = InnoDB;
+  UNIQUE KEY `unindex` (`classcation`,`date`,`regioname`),
+  KEY `classcation` (`classcation`),
+  KEY `date` (`date`),
+  KEY `regioname` (`regioname`)
+) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8
+
+
+
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
