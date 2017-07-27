8bbe61aa7c36d066263796e8a38917ef6d9211ec
zhenyu7.16 <260787342@qq.com>
2017-07-27 14:52:55 +0800

    07-27

--- SpringBootPro/sql/sales_volume.sql
+++ SpringBootPro/sql/sales_volume.sql
@@ -12,3 +12,42 @@ CREATE TABLE  IF NOT EXISTS   `dbgirl`.`sales_volume` (
   KEY `date` (`date`),
   KEY `regioname` (`regioname`)
 ) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8
+
+
+
+-----------------------------------------------------
+DROP PROCEDURE IF EXISTS `pro_salse_query`;  
+CREATE PROCEDURE pro_salse_query(OUT p_count INT)
+BEGIN
+	#Routine body goes here...
+	
+SET @sql = NULL;
+	SELECT
+	 GROUP_CONCAT(DISTINCT
+		CONCAT(
+		 'MAX(CASE st.date WHEN ''',st.date,''' THEN  st.amount ELSE 0 END ) 
+
+      AS ''',st.date ,''' '
+		)
+	 ) INTO @sql
+	FROM sales_volume st ;
+
+
+SET @sql = CONCAT(' Create TEMPORARY table  tab1  (Select st.regioname, st.classcation, ', @sql, 
+							'  From sales_volume st 
+							
+							Group by  st.classcation  ,  st.regioname   ) ; ');
+
+	PREPARE stmt FROM @sql;
+	EXECUTE stmt;
+	DEALLOCATE PREPARE stmt;
+
+  desc tab1 ;
+  set p_count =  FOUND_ROWS(); 
+
+-- select * from tab1  ORDER BY classcation ;
+
+DROP TABLE tab1 ;
+
+END
+-------------------------
