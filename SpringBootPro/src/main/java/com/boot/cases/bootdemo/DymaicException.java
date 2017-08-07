be376039a9736da27c86d161a6936d5c54dcb9a7
zhenyu7.16 <260787342@qq.com>
2017-08-07 21:39:43 +0800

    08/07

--- SpringBootPro/src/main/java/com/boot/cases/bootdemo/DymaicException.java
+++ SpringBootPro/src/main/java/com/boot/cases/bootdemo/DymaicException.java
@@ -1,19 +1,21 @@
 package com.boot.cases.bootdemo;
 
 /**
- * 自定义Exception 必须要继承RuntimeException，因为Spring只对RuntimeException的异常进行事务的回滚
+ * 自定义Exception 必须要继承RuntimeException，
+ * 因为Spring只对RuntimeException的异常进行事务的回滚
  * @author Administrator
  *
  */
 public class DymaicException extends RuntimeException {
 	
+	private static final long serialVersionUID = -4431795741125420300L;
+
+	private Integer code ;
 	
 	public DymaicException(ResultEnum resultEnum ) {
 		super(resultEnum.getMessage());
 		this.code = resultEnum.getCode();
 	}
-
-	private Integer code ;
 
 	public Integer getCode() {
 		return code;
